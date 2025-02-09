97534a57ac43df07ac262422213cec95b84ce1a8
Galindo Leite <112688705+galindoptbr@users.noreply.github.com>
2025-02-09 18:06:40 +0000

    adjust menu mobile

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -9,18 +9,14 @@ import logo from "../assets/images/siteparastand-logo.png";
 
 export const Navbar: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const pathname = usePathname(); // Hook para pegar a rota atual
+  const pathname = usePathname();
 
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
   const handleNavigation = (hash: string) => {
     if (pathname !== "/") {
-      // Se não estiver na landing page, redireciona para a home
       window.location.href = `/${hash}`;
     } else {
-      // Scroll para o elemento da página inicial
       const element = document.querySelector(hash);
       if (element) {
         element.scrollIntoView({ behavior: "smooth" });
@@ -29,16 +25,62 @@ export const Navbar: React.FC = () => {
   };
 
   return (
-    <div className="w-full bg-zinc-800 shadow-lg fixed top-0 left-0 z-50">
-      <div className="flex justify-between items-center m-auto p-4 max-w-[1200px]">
-        {/* Logo */}
+    <div className="w-full fixed top-0 left-0 z-50">
+      {/* Navbar Desktop */}
+      <div className="hidden lg:block bg-zinc-800">
+        <div className="max-w-[1200px] mx-auto px-8 py-4 flex justify-between items-center">
+          {/* Logo */}
+          <Link href="/" replace>
+            <Image src={logo} alt="Logo" className="w-48" />
+          </Link>
+          {/* Links de Navegação */}
+          <ul className="flex gap-8 items-center font-semibold text-white">
+            <li>
+              <a
+                onClick={() => handleNavigation("#inicio")}
+                className="hover:text-gray-300 transition-all cursor-pointer"
+              >
+                Início
+              </a>
+            </li>
+            <li>
+              <Link
+                href="/pages/about"
+                className="hover:text-gray-300 transition-all cursor-pointer"
+              >
+                Sobre Nós
+              </Link>
+            </li>
+            <li>
+              <a
+                onClick={() => handleNavigation("#servicos")}
+                className="hover:text-gray-300 transition-all cursor-pointer"
+              >
+                Serviços
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => handleNavigation("#dominio")}
+                className="hover:text-gray-300 transition-all cursor-pointer"
+              >
+                Domínio
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Navbar Mobile */}
+      <div className="bg-zinc-800 flex items-center justify-between p-4 max-w-[1200px] mx-auto lg:hidden">
+        {/* Logo Mobile */}
         <Link href="/" replace>
-          <Image className="w-44 rounded-lg" src={logo} alt="logo" />
+          <Image src={logo} alt="Logo" className="w-40" />
         </Link>
-        {/* Botão para menu mobile */}
+        {/* Botão para abrir/fechar o menu */}
         <button
           onClick={toggleMobileMenu}
-          className="lg:hidden text-white focus:outline-none"
+          className="text-white focus:outline-none"
         >
           {isMobileMenuOpen ? (
             <svg
@@ -72,80 +114,59 @@ export const Navbar: React.FC = () => {
             </svg>
           )}
         </button>
-        {/* Navbar para telas grandes */}
-        <ul className="hidden lg:flex gap-8 items-center font-semibold text-white">
-          <li>
-            <a
-              onClick={() => handleNavigation("#inicio")}
-              className="hover:text-gray-300 transition-all cursor-pointer"
-            >
-              Início
-            </a>
-          </li>
-          <li>
-            <Link
-              href={"/pages/about"}
-              className="hover:text-gray-300 transition-all cursor-pointer"
-            >
-              Sobre Nós
-            </Link>
-          </li>
-          <li>
-            <a
-              onClick={() => handleNavigation("#servicos")}
-              className="hover:text-gray-300 transition-all cursor-pointer"
-            >
-              Serviços
-            </a>
-          </li>
-          <li>
-            <a
-              onClick={() => handleNavigation("#dominio")}
-              className="hover:text-gray-300 transition-all cursor-pointer"
-            >
-              Domínio
-            </a>
-          </li>
-        </ul>
-        {/* Navbar para telas pequenas */}
-        {isMobileMenuOpen && (
-          <ul className="lg:hidden bg-zinc-800 fixed top-20 left-0 w-full z-10 flex flex-col gap-4 p-4">
-            <li>
-              <a
-                onClick={() => {
-                  handleNavigation("#inicio");
-                  setIsMobileMenuOpen(false);
-                }}
-                className="text-gray-100 hover:text-gray-300 cursor-pointer"
-              >
-                Início
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  handleNavigation("#projetos");
-                  setIsMobileMenuOpen(false);
-                }}
-                className="text-gray-100 hover:text-gray-300 cursor-pointer"
-              >
-                Desenvolvemos seu Site
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  handleNavigation("#dominio");
-                  setIsMobileMenuOpen(false);
-                }}
-                className="text-gray-100 hover:text-gray-300 cursor-pointer"
-              >
-                Verificar Domínio
-              </a>
-            </li>
-          </ul>
-        )}
       </div>
+
+      {/* Menu Mobile */}
+      <ul
+        className={`lg:hidden bg-zinc-800 fixed inset-x-0 top-20 z-10 flex flex-col items-center gap-8 transition-all duration-300 p-10 ${
+          isMobileMenuOpen
+            ? "opacity-100 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <li>
+          <a
+            onClick={() => {
+              handleNavigation("#inicio");
+              setIsMobileMenuOpen(false);
+            }}
+            className="hover:text-gray-300 transition-all cursor-pointer font-semibold text-white"
+          >
+            Início
+          </a>
+        </li>
+        <li>
+          <Link
+            href="/pages/about"
+            className="hover:text-gray-300 transition-all cursor-pointer font-semibold text-white"
+            onClick={() => setIsMobileMenuOpen(false)}
+          >
+            Sobre Nós
+          </Link>
+        </li>
+        <li>
+          <a
+            onClick={() => {
+              handleNavigation("#servicos");
+              setIsMobileMenuOpen(false);
+            }}
+            className="hover:text-gray-300 transition-all cursor-pointer font-semibold text-white"
+          >
+            Serviços
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={() => {
+              handleNavigation("#dominio");
+              setIsMobileMenuOpen(false);
+            }}
+            className="hover:text-gray-300 transition-all cursor-pointer font-semibold text-white"
+          >
+            Domínio
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
